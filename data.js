10fff13c5f8573c260dc69ab17728611ed88e45b
Jasmin Dedic <jdedic1999@gmail.com>
2022-10-20 15:20:43 +0200

    Finished first version of content section

---
+++ data.js
@@ -0,0 +1,38 @@
+let products = [
+    {
+        "img": "./images/product_1.png",
+        "desc": "Hand Cream",
+        "price": 120,
+        "amount": 0
+    },
+    {
+        "img": "./images/product_2.png",
+        "desc": "Face Cream",
+        "price": 110,
+        "amount": 0
+    },
+    {
+        "img": "./images/product_3.png",
+        "desc": "Body Lotion",
+        "price": 105,
+        "amount": 0
+    },
+    {
+        "img": "./images/product_4.png",
+        "desc": "Ageing Cream",
+        "price": 260,
+        "amount": 0
+    },
+    {
+        "img": "./images/product_5.png",
+        "desc": "Eye Cream",
+        "price": 150,
+        "amount": 0
+    },
+    {
+        "img": "./images/product_6.png",
+        "desc": "Body Cream",
+        "price": 220,
+        "amount": 0
+    },
+];
--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 // Get elements
-
+let productContainer = document.querySelector(".products-container");
 
 
 // init function
 function init() {
-    generateNavbar()
+    generateNavbar();
+    render();
 }
 
 // render function
 function render() {
-
+    generateProducts();
 }
 
+// Function to generate content (products)
+function generateProducts() {
+    productContainer.innerHTML = "";
+    for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+        productContainer.innerHTML += createProducts(product);
+    }
+}
+
+// Function to create content (html)
+function createProducts(product) {
+    return /* html */`
+    <div class="product">
+        <img class="product-img" src="${product["img"]}">
+    <div class="product-desc">
+        <h3>${product["desc"]}</h3>
+    </div>
+    <div class="price-container">
+        <p class="price">${product["price"]}$</p>
+    </div>
+    <div class="product-buttons">
+        <div class="buy-btn-container">
+            <button type="button" class="buy-btn">Buy</button>
+            <span class="amount">0</span>
+        </div>
+        <div class="increase-decrease">
+            <i class="fa-solid fa-plus"></i>
+            <i class="fa-solid fa-minus"></i>
+        </div>
+    </div>
+</div>
+    `;
+}
